6c6756b20feea8722f30ae3aa769fce06c4c89db
Vaibhav Satam <vaibhav.satam@accionlabs.com>
2024-06-05 21:06:37 +0530

    added patient seeder

--- components/feature/header.tsx
+++ components/feature/header.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import MountainIcon from '@/public/icons/MountainIcon';
 import { signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -14,6 +13,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '../ui/dropdown-menu';
+import bg from '/public/images/logo.png';
 
 const Header = () => {
   const session = useSession();
@@ -25,11 +25,11 @@ const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-10 bg-white/90 backdrop-blur-md px-4 md:px-6 h-16 flex items-center justify-between dark:bg-gray-950/90">
       <Link
-        href="#"
+        href="/"
         className="mr-6 flex items-center gap-2 text-lg font-semibold md:text-base"
         prefetch={false}
       >
-        <MountainIcon className="w-6 h-6" />
+        <Image src={bg} alt="hero image" height={64} width={152} priority />
         <span className="sr-only">Acme Inc</span>
       </Link>
       {session.status === 'unauthenticated' && (
@@ -45,16 +45,19 @@ const Header = () => {
         <div className="ml-auto flex items-center gap-4 md:gap-2 lg:gap-4">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Avatar className="h-9 w-9">
-                <Image
-                  height={64}
-                  width={64}
-                  src="/placeholder.svg"
-                  alt="profile"
-                />
-                {/* <AvatarFallback>JP</AvatarFallback> */}
-                <span className="sr-only">Toggle user menu</span>
-              </Avatar>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-9 w-9">
+                  <Image
+                    src="/placeholder.svg"
+                    height={64}
+                    width={64}
+                    alt="profile"
+                  />
+                </Avatar>
+                <div className="text-sm font-medium cursor-pointer">
+                  {session.data.user.name}
+                </div>
+              </div>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56">
               <DropdownMenuLabel>
